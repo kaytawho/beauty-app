e0696fb56df929846206563d63f32260cc817a79
kayta <kaytahackman@gmail.com>
2022-01-15 16:54:38 +1100

    updating DB_URL

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template
 import os
 import psycopg2
 
-DB_URL = os.environ.get("DATABASE_URL", "dbname=beauty_app")
+# DB_URL = os.environ.get("DATABASE_URL", "dbname=beauty_app")
 SECRET_KEY = os.environ.get("SECRET_KEY", "password")
 
 app = Flask(__name__)
